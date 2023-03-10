ea9c5f9ceab875288b4bc0d0647bc90ff8834a80
solidmike149 <michele.terrani@protonmail.com>
2023-03-10 15:25:19 +0100

    Projectile Animation

---
+++ Source/Gilt/Private/Actors/GiltCurveProjectile.cpp
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actors/GiltCurveProjectile.h"
+
+
+// Sets default values
+AGiltCurveProjectile::AGiltCurveProjectile()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AGiltCurveProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if(!Curve.GetRichCurve())
+	{
+		SetActorTickEnabled(false);
+	}
+}
+
+// Called every frame
+void AGiltCurveProjectile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	Time += DeltaTime;
+
+	if (Curve.GetRichCurveConst() && Curve.GetRichCurveConst()->GetNumKeys() > 0)
+	{
+		float YValue = Curve.GetRichCurveConst()->Eval(Time);
+
+		FVector CurrentLocation = GetActorLocation();
+
+		FVector NewLocation = FVector(CurrentLocation.X, CurrentLocation.Y + YValue, CurrentLocation.Z);
+		SetActorLocation(NewLocation, true);
+	}
+}
---
+++ Source/Gilt/Public/Actors/GiltCurveProjectile.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GiltProjectileBase.h"
+#include "GiltCurveProjectile.generated.h"
+
+UCLASS()
+class GILT_API AGiltCurveProjectile : public AGiltProjectileBase
+{
+	GENERATED_BODY()
+
+	AGiltCurveProjectile();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Projectile Animation")
+	FRuntimeFloatCurve Curve;
+
+	UPROPERTY(BlueprintReadWrite)
+	bool bHasHitPlayer;
+
+private:
+
+	float Time;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
